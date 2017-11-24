159fbfa441b29e9eb3e24c0fa31c0c8efeb0de9a
Mohamed Moallim <mohamedmoallim31@gmail.com>
2017-11-24 19:48:49 +0000

    24/11/2017

--- Desktop/4th year project/generateInitialConditions.m
+++ Desktop/4th year project/generateInitialConditions.m
@@ -1,9 +1,10 @@
 %File will save intitial solution to xsl spreadsheet 
 numberOfParticles = 8;
-numberOfSteps = 1e3;
-timeStep = 1/1000;
+numberOfSteps = 20;
+timeStep = 1/200;
 
 p = particleChainSimulation(timeStep, numberOfParticles, numberOfSteps);
 
-filename = 'solution.xlsx';
-xlswrite(filename, p{1});
+delete 'solution.xlsx';
+xlswrite('solution.xlsx', p{1}, 'p');
+xlswrite('solution.xlsx', p{2}, 'q');
