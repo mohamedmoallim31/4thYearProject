d7fdd5657e49113363b4b6a62a06e6212326fc73
Tom Hudson <thudso@gmail.com>
2018-03-13 15:38:43 +0000

    Various bugfixes

--- Desktop/4th year project/1DChain/generatef_0.m
+++ Desktop/4th year project/1DChain/generatef_0.m
@@ -1,19 +1,17 @@
 function [ f_0 ] = generatef_0(B, numberOfParticles,numberOfSteps,timeStep, u0)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-    eqV = zeros(numberOfParticles,1);
     eqU = zeros(numberOfParticles,1);
     eqU(1) = u0(1);
     for i = 2:numberOfParticles
-        eqU(i) = eqU(i)+1;
+        eqU(i) = eqU(i-1)+1;
     end
     
     C = solveForC(B, numberOfParticles, numberOfSteps, timeStep);
-    S = solveForS(B, numberOfParticles, numberOfSteps, timeStep);
     
     f_0 = cell(numberOfSteps, 1);
     for j = 1:numberOfSteps
-        f_0{j} = C{j}*(eqU) + S{j}*(eqV);
+        f_0{j} = C{j}*eqU;
     end
 end
 
