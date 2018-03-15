03672b6f4c764fe9df61dd56229aa07dea2a73f4
Tom Hudson <thudso@gmail.com>
2018-03-15 13:27:53 +0000

    Fixed generateR

--- Desktop/4th year project/1DChain/coarseCompare.m
+++ Desktop/4th year project/1DChain/coarseCompare.m
@@ -1,6 +1,6 @@
 numberOfParticles = 8;
-numberOfSteps = 80000;
-timeStep = 1/80000;
+numberOfSteps = 8000;
+timeStep = 1/8000;
 t = linspace(0,1,numberOfSteps);
 B = [1,0,0,0,0,0,0,0; 0,0,0,0,1,0,0,0; 0,0,0,0,0,0,0,1];
 sizeB = size(B);
--- Desktop/4th year project/1DChain/generateR.m
+++ Desktop/4th year project/1DChain/generateR.m
@@ -4,10 +4,12 @@ function R = generateR(B, numberOfParticles)
     %D is as defined on Li's paper
     D_0 = -2*72;
     D_mod1 = 72;
-    D = -full(gallery('tridiag', numberOfParticles, D_mod1,D_0,D_mod1));
+    D = full(gallery('tridiag', numberOfParticles, D_mod1,D_0,D_mod1));
     D(1,1) = -72;
     D(numberOfParticles,numberOfParticles) = -72;
+    D=-D;
+    Dinv = pinv(D);
     transposeB = transpose(B);
-    matrix = B*(D^-1)*transposeB;
-    R = D\(transposeB/matrix);
+    matrix = (B*Dinv*transposeB);
+    R = (Dinv*transposeB)/matrix;
 end
