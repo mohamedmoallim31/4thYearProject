233c3100bf5b87a95ae65e77d780f16e7a2de5c5
Tom Hudson <thudso@gmail.com>
2018-03-27 10:16:02 +0100

    Fixed linear and angular momenta in initial conditions

--- Desktop/4th year project/2DLattice/randomInitilizer.m
+++ Desktop/4th year project/2DLattice/randomInitilizer.m
@@ -1,9 +1,6 @@
 function [p0, q0] = randomInitilizer(gridDimension)
     n = gridDimension;
     p0 = 0.2*randn(n*n,2);
-    meanOfp0 = mean(p0,1);
-    p0 = p0 - meanOfp0;
-    p0 = p0';
     
     %note randn returns a number from the standard normal distribution
     q = zeros(n*n, 2);
@@ -15,5 +12,21 @@ function [p0, q0] = randomInitilizer(gridDimension)
         end
     end
     q0 = transpose(q);    
+    
+    % Now construct projection which sets both total linear and angular
+    % momentum to zero
+    L = zeros(3,2*n*n);
+    L(1,1:2:end) = 1; % x momentum
+    L(2,2:2:end) = 1; % y momentum
+    L(3,1:2:end) = q0(2,:);
+    L(3,2:2:end) = -q0(1,:);
+    
+    Lproj = null(L);
+    Lproj = Lproj*Lproj';
+    
+    p0 = myreshape(p0',n);
+    p0 = (Lproj*p0')';
+    p0 = reverseReshape(p0,n);
+    
 end
 
